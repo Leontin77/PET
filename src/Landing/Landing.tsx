f595ef02d7ff62d17f6d27c3a89a7abeb8ae1863
Eugen <fredarkq18@gmail.com>
2023-11-02 13:27:42 +0200

    detailes (secPage), header changes

--- src/Landing/Landing.tsx
+++ src/Landing/Landing.tsx
@@ -4,6 +4,7 @@ import dron from "../images/dronBG.png";
 import headerText from "../images/headerText.png"
 import { useState } from "react";
 import { Content } from "Content";
+import { Details } from "DetaIls";
 export interface ILandingProps {}
 
 export const Landing = ({ ...props }: ILandingProps): JSX.Element => {
@@ -19,9 +20,9 @@ export const Landing = ({ ...props }: ILandingProps): JSX.Element => {
         <div className="wrapper_header-row">
           <img className="wrapper_header-row_logo" src={logo} />
           <div className="wrapper_header-row_navigation">
-            <div className={`wrapper_header-row_navigation-item ${tab === 8 && 'active'}`} onClick={() => setTab(8)}>DRON 1</div>
-            <div className={`wrapper_header-row_navigation-item ${tab === 10 && 'active'}`} onClick={() => setTab(10)}>DRON 2</div>
-            <div className={`wrapper_header-row_navigation-item ${tab === 12 && 'active'}`} onClick={() => setTab(12)}>ABOUT</div>
+            <div className={`wrapper_header-row_navigation-item ${tab === 8 && 'active'}`} onClick={() => setTab(8)}>FPV 8</div>
+            <div className={`wrapper_header-row_navigation-item ${tab === 10 && 'active'}`} onClick={() => setTab(10)}>FPV 10</div>
+            <div className={`wrapper_header-row_navigation-item ${tab === 12 && 'active'}`} onClick={() => setTab(12)}>КОНТАКТИ</div>
 
           </div>
           <img className="wrapper_header-row_text" src={headerText} />
@@ -29,6 +30,7 @@ export const Landing = ({ ...props }: ILandingProps): JSX.Element => {
         </div>
       </div>
       <Content tab={tab}/>
+      <Details tab={tab}/>
     </div>
   );
 };
